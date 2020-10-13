d97aa944b345cffee906bf113c659226c2d28154
Takatoshi Miura <takatoshi052031@gmail.com>
2020-10-13 09:02:49 +0900

    [Update] NSLogを追記。

--- 014-ObjcSwitch/ViewController.m
+++ 014-ObjcSwitch/ViewController.m
@@ -27,8 +27,14 @@
 // SwitchのON/OFFで背景色を切り替える
 - (IBAction)switchOnOff:(UISwitch *)sender {
     if (sender.on) {
+        NSLog(@"SwitchがONになりました");
+        NSString *message = @"ON";
+        NSLog(@"Switchが%@になりました",message);
         self.view.backgroundColor = [UIColor systemBlueColor];
     } else {
+        NSLog(@"SwitchがOFFになりました");
+        NSString *message = @"OFF";
+        NSLog(@"Switchが%@になりました",message);
         self.view.backgroundColor = [UIColor systemGrayColor];
     }
 }
